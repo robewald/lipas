70bd176bfe71273deac82e7b8c7c259735707b5b
Valtteri Harmainen <valtteri.harmainen@gmail.com>
2018-09-21 12:48:35 +0300

    Refactored horrible function chain to better one

--- webapp/src/cljs/lipas/ui/map/subs.cljs
+++ webapp/src/cljs/lipas/ui/map/subs.cljs
@@ -15,23 +15,14 @@
  (fn [db _]
    (-> db :map :filters)))
 
-(def type-codes
-  {:ice-stadium   #{2510 2520}
-   :swimming-pool #{3110 3130}})
-
 (re-frame/reg-sub
  ::geometries
  :<- [:lipas.ui.sports-sites.subs/latest-sports-site-revs]
  :<- [::filters]
  (fn [[sites filters] _]
-   (let [type-codes (->> filters
-                         vec
-                         (filter (comp true? second))
-                         (map first)
-                         (select-keys type-codes)
-                         vals
-                         (reduce concat)
-                         set)]
+   (let [type-codes (cond-> #{}
+                      (:ice-stadium filters) (into #{2510 2520})
+                      (:swimming-pool filters) (into #{3110 3130}))]
      (->> sites
           vals
           (filter (comp type-codes :type-code :type))
